36d8492c86de490c32bb7b29224c59e08baf9e95
dreyescat <dreyes.cat@gmail.com>
2022-12-02 18:25:35 +0100

    Solve 02 Rock Paper Scissor second half

--- 02-rock-paper-scissors/src/main.rs
+++ 02-rock-paper-scissors/src/main.rs
@@ -15,10 +15,28 @@ fn main() {
         ("C Y", 2 + 0),
         ("C Z", 3 + 3),
     ]);
+    // X = Need to Lose, Y = Need to Draw, Z = Need to Win
+    let scores_2 = HashMap::from([
+        ("A X", *scores.get("A Z").unwrap()),
+        ("A Y", *scores.get("A X").unwrap()),
+        ("A Z", *scores.get("A Y").unwrap()),
+        ("B X", *scores.get("B X").unwrap()),
+        ("B Y", *scores.get("B Y").unwrap()),
+        ("B Z", *scores.get("B Z").unwrap()),
+        ("C X", *scores.get("C Y").unwrap()),
+        ("C Y", *scores.get("C Z").unwrap()),
+        ("C Z", *scores.get("C X").unwrap()),
+    ]);
     let score = io::stdin()
         .lines()
         .into_iter()
-        .map(|line| scores.get(line.unwrap().as_str()).unwrap())
-        .sum::<u32>();
-    println!("{}", score);
+        .map(|line| {
+            let key = line.unwrap();
+            (
+                scores.get(key.as_str()).unwrap(),
+                scores_2.get(key.as_str()).unwrap(),
+            )
+        })
+        .fold((0, 0), |(acc1, acc2), (s1, s2)| (acc1 + s1, acc2 + s2));
+    println!("{:?}", score);
 }
